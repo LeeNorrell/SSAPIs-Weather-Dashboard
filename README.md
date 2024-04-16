466262c1ce9ea0f1f47a4510d2a428b4924dd224
LeeNorrell <156039272+LeeNorrell@users.noreply.github.com>
2024-04-15 22:53:17 -0500

    needs work in javascript with the five day forecast as well as the recent searches

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # SSAPIs-Weather-Dashboard
+
+Description 
+
+This page will help you find the weather in any city and keep track of your searches for quick acces.
+You will also be provided with a five day forecast.
+
+Author
+Lee Norrell
+bootstrap
+jquery
+other info off the web
+
+license
+see license
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,163 @@
-function myFunction() {
-    // Declare variables
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("mySearch");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myMenu");
-    li = ul.getElementsByTagName("li");
-  
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("a")[0];
-      if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
-      } else {
-        li[i].style.display = "none";
-      }
-    }
-  }
+const apiKey = 'a25eedf723e2ad42229e0aa7593d6137'
+const apiLatLongWeatherInfo = ({ lat, lon }) => `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+const apiLatLongWeatherForecast = ({ lat, lon }) => `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+const apiLatLongFromCityName = ({ cityName }) => (`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`);
+
+function getnextId() {
+  const currentId = localStorage.getItem('nextId') || 0;
+  const newId = +currentId + 1;
+  localStorage.setItem('nextId', newId);
+  return newId;
+}
+
+function createIdFromFields(IdName){
+  const newId ={
+    IdName: IdName,
+  };
+  return newId;
+}
+
+function createId(props) {
+  const {IdName} = props;
+  const boxHtml =`
+  <li class="list"><a>${IdName}</a></li>
+  `;
+  return boxHtml;
+}
+
+
+function handleSearchId(event) {
+  event.preventDefault();
+  const currentId =getnextId();
+  const newId = createId(currentId);
+
+  const Id = readId();
+  Id.push(newId);
+  writeId(Id);
+
+  const boxHtml = createId(newId);
+  $('#Id').append(boxHtml);
+}
+
+function readId() {
+  const currentId = JSON.parse(localStorage.getItem('Id')) || [];
+  return currentId;
+}
+
+function writeId(Id) {
+  const storeId = JSON.stringify(Id);
+ return localStorage.setItem('Id', storeId);
+}
+
+function renderId() {
+  const Id = readId();
+  Id.forEach((Id) => {
+    const boxHtml = createId(Id);
+    $('#Id').append(boxHtml);
+  });
+}
+
+
+async function getLocationFromCityName(cityName) {
+  const apiUrl = apiLatLongFromCityName({ cityName });
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  const {
+    name,
+    lat,
+    lon,
+  } = data[0];
+  return ({name, lat, lon});
+}
+
+async function getWeatherFromCityInfo(cityInfo) {
+  const apiUrl = apiLatLongWeatherInfo(cityInfo);
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  return data;
+}
+
+
+async function getWeatherForecastFromCityInfo(cityInfo) {
+  const apiUrl = apiLatLongWeatherForecast(cityInfo);
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  return data;
+}
+
+function currentWeatherTemplate(weatherInfo) {
+  const {
+    name,
+    main: {
+      temp,
+      humidity,
+    },
+    wind: {
+      speed,
+    },
+
+  } = weatherInfo;
+
+  return `
+    <div class="currentWeather">
+      <h2>${name}</h2>
+      <p>Temp: ${temp}</p>
+      <p>Wind: ${speed} MPH</p>
+      <p>Humidity: ${humidity}%</p>
+    </div>
+  `;
+}
+
+function forecastWeatherTemplate() {
+  for(let i = 0; i < 5; i++) {
+    const {
+      main: {
+        temp,
+        humidity,
+      },
+      wind: {
+        speed,
+      },
+    } = forecastInfo.list[i];
+    return `
+        /*<div class = "forecast">
+          <p>Temp: ${temp}</p>
+          <p>Wind: ${speed} MPH</p>
+          <p>Humidity: ${humidity}%</p>
+        <div>
+        `;
+}
+}
+
+console.log(forecastInfo);
+const weatherConditionsElement = $("#currentWeatherConditions");
+function displayCurrentWeatherConditions(weatherInfo) {
+  weatherConditionsElement.empty();
+  weatherConditionsElement.append(currentWeatherTemplate(weatherInfo))
+}
+
+const forecastInfoElement = $("#forecastInfo");
+function displayWeatherForecast(forecastInfo){
+  forecastInfoElement.empty();
+  forecastInfo.list.forEach(function(dayForecastInfo) {
+    forecastInfoElement.append(forecastWeatherTemplate(dayForecastInfo));
+  });
+};
+
+const searchField = $('#mySearch');
+function getWeatherCityValue() {
+  return searchField.val();
+}
+
+$(document).ready(function() {
+  searchField.val('Atlanta');
+  $('#searchButton').click(async function() {
+    const cityInfo = await getLocationFromCityName(searchField.val());
+
+    const weatherInfo = await getWeatherFromCityInfo(cityInfo);
+    displayCurrentWeatherConditions(weatherInfo);
+
+    const weatherForecast = await getWeatherForecastFromCityInfo(cityInfo);
+    displayWeatherForecast(weatherForecast);
+  });
+});
